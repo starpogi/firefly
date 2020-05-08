774c8aeae519bcd49d76024417e78dd948713ced
Javier Onglao <onglao@google.com>
2020-05-07 17:42:31 -0700

    Leaflet Setup - Libraries for Leaflet and React Lifecycle Updates - Adding Flow for React - Flow types for Map container

--- ui/src/containers/Map/Map.test.js
+++ ui/src/containers/Map/Map.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Map from './Map';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Map />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
